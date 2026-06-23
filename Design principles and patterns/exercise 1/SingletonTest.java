f22e89e33bcdff9fea86979436e5cb255d7f6a28
JyothikaPJ <jyothikapja25@gmail.com>
2026-06-23 09:05:08 +0530

    Add exercise 1 files

---
+++ Design principles and patterns/exercise 1/SingletonTest.java
@@ -0,0 +1,21 @@
+
+public class SingletonTest {
+
+    public static void main(String[] args) {
+
+        // Get Logger instance
+        Logger logger1 = Logger.getInstance();
+        logger1.log("First message");
+
+        // Get Logger instance again
+        Logger logger2 = Logger.getInstance();
+        logger2.log("Second message");
+
+        // Check if both references point to the same object
+        if (logger1 == logger2) {
+            System.out.println("Only one Logger instance exists.");
+        } else {
+            System.out.println("Multiple Logger instances exist.");
+        }
+    }
+}
